8cd6b57463e5006a8ad24b69646eafa7ead446fe
MorganWilliamson <mwilliamson7695@gmail.com>
2020-11-09 15:40:32 -0600

    Testing App.js

---
+++ src/App.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen, fireEvent, wait } from "@testing-library/react";
+import userEvent from "@testing-library/react";
+import App from "./App"
+
+test("App renders without errors.", () => {
+    render(<App />);
+});
+
+test("Dropdown menu renders.", async () => {
+    render(<App />);
+
+    const dropdown = await screen.findByText(/select a season/i);
+    expect(dropdown).toBeEnabled();
+});
+
+test("Dropdown menu populates info.", async () => {
+    render(<App />);
+
+    const dropdown = await screen.findByText(/select a season/i);
+    fireEvent.click(dropdown);
+
+    const seasons = await screen.queryByText(/Season 1/i);
+    expect(seasons).toBeNull();
+})
